59f1f9a294ffb3e1873f339b004d3969b4f918fb
ziftikhar101 <zainabi@umich.edu>
2023-12-11 13:36:17 -0500

    Visualization 1 & 2

--- calc_visualizations.py
+++ calc_visualizations.py
@@ -51,3 +51,64 @@ def main():
 
     # Close the database connection
     # conn.close()
+ # Plotting Visualization 1
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    ax1.set_xlabel("Month (2023)")
+    ax1.set_ylabel("Avg Temperature (°C)", color="tab:blue")
+    ax1.plot(
+        df_viz1["Month"].values,
+        df_viz1["AvgTemperature"].values,
+        color="tab:blue",
+        label="Avg Temperature",
+    )
+    ax1.tick_params(axis="y", labelcolor="tab:blue")
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.set_ylabel(
+        "Avg Stock Price ($)", color="tab:red"
+    )  # we already handled the x-label with ax1
+    ax2.plot(
+        df_viz1["Month"].values,
+        df_viz1["AvgStockPrice"].values,
+        color="tab:red",
+        label="Avg Stock Price",
+    )
+    ax2.tick_params(axis="y", labelcolor="tab:red")
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.title("Average Temperature in Ann Arbor vs Stock Price (2023)")
+    plt.show()
+
+    # Plotting Visualization 2
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    ax.bar(
+        df_viz2["Month"], df_viz2["AvgPM10"], color="skyblue", width=0.4, label="Avg PM10"
+    )
+    ax.bar(
+        df_viz2["Month"],
+        df_viz2["AvgPM25"],
+        color="orange",
+        width=0.4,
+        label="Avg PM2.5",
+        bottom=df_viz2["AvgPM10"],
+    )
+    ax.set_xlabel("Month (2023)")
+    ax.set_ylabel("Average PM10 and PM2.5 (μg/m³)")
+    ax.tick_params(axis="y")
+
+    ax2 = ax.twinx()
+    ax2.plot(
+        df_viz2["Month"].values,
+        df_viz2["AvgStockPrice"].values,
+        color="green",
+        label="Avg Stock Price",
+    )
+    ax2.set_ylabel("Avg Stock Price ($)")
+    ax2.tick_params(axis="y", labelcolor="green")
+
+    fig.tight_layout()
+    plt.title("Air Quality (PM10 & PM2.5) in Ann Arbor and Stock Price (2023)")
+    plt.legend(loc="upper left")
+    plt.show()
